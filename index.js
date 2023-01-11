8dc532237635650fbf67b4616a5cdaa273609c95
Okkun555 <y.okuno.btm@gmail.com>
2023-01-11 22:50:28 +0900

    feat:postsを追加

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ const typeDefs = gql`
     id: ID!
     name: String!
     email: String!
+    myPosts: [Post]
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    userId: ID!
   }
 
   type Query {
     hello(name: String!): String
     users: [User]
     user(id: ID!): User
+    posts: [Post]
   }
 `;
 
@@ -25,10 +34,16 @@ const resolvers = {
       return res.data;
     },
     user: async (parent, args) => {
-      const res = await axios.get(
+      let response = await axios.get(
         `https://jsonplaceholder.typicode.com/users/${args.id}`
       );
-      return res.data;
+      let user = response.data;
+      response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      const myPosts = response.data.filter((post) => post.userId == args.id);
+      user = Object.assign({}, user, {
+        myPosts: myPosts,
+      });
+      return user;
     },
   },
 };
