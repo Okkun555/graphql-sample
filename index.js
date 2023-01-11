e48fd8e3f0eeb7f85f015a6aae5739b83818feea
Okkun555 <y.okuno.btm@gmail.com>
2023-01-11 22:33:14 +0900

    feat:userスキーマを作成

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ const { ApolloServer, gql } = require("apollo-server");
 
 // スキーマの設定
 const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
   type Query {
     hello(name: String!): String
+    users: [User]
+    user(id: ID!): User
   }
 `;
+
+const users = [
+  { id: "1", name: "Kana Togo", email: "togo@sample.com" },
+  { id: "2", name: "Tom Doe", email: "tom@test.com" },
+];
 
 // リゾルバの設定
 const resolvers = {
   Query: {
     hello: (parent, args) => `Hello ${args.name}`,
+    users: () => users,
+    user: (parent, args) => users.find((user) => user.id === args.id),
   },
 };
 
